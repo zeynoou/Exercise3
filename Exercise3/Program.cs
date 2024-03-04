c8d1c78b33e5c6f450c258761d0d200398e5b5c6
Zeynab <Zeynab@ZEYNAB>
2024-03-04 10:43:29 +0330

    Add project files.

---
+++ Exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age :");
+            string age = Console.ReadLine();
+            var result1 = Convert.ToInt32(age);
+            var result2 = get_age(result1);
+            Console.WriteLine("Your age in day is:" +  result2);
+        }
+
+        public static int get_age(int a)
+        {
+            int age;
+            age = a * 365;
+            return age;
+        }
+    }
+}
